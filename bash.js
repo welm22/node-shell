29e17d44a96be577960ee9bb45ae31e38c7a6f43
William Xu <williamxu97@gmail.com>
2019-11-11 15:46:03 -0500

    fixed cat

--- bash.js
+++ bash.js
@@ -1,18 +1,20 @@
 const pwd = require('./pwd.js');
 const ls = require('./ls.js');
+const cat = require('./cat.js');
 
 process.stdout.write('prompt > ');
 process.stdin.on('data', data => {
-  const cmd = data.toString().trim().split(' ');
+  const cmd = data
+    .toString()
+    .trim()
+    .split(' ');
   if (cmd[0] === 'pwd') {
     pwd();
   }
-  if(cmd[0] === 'ls'){
+  if (cmd[0] === 'ls') {
     ls();
   }
-  if(cmd[0] === 'cat'){
+  if (cmd[0] === 'cat') {
     cat([...cmd.slice(1)]);
   }
-
 });
-
